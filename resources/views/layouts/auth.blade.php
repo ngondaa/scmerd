14c233ebe66f721beb9dcc932de671c4a3b8330a
ngondaa <113019728+ngondaa@users.noreply.github.com>
2026-09-04 15:48:08 +0200

    .

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,7 +1,7 @@
-<x-layouts::auth.card> :title="$title ?? null">
+<x-layouts::auth.split :title="$title ?? null">
     <div class="min-h-screen bg-white px-4 py-10 text-black">
         <div class="mx-auto max-w-md pt-8 text-black">
             {{ $slot }}
         </div>
     </div>
-</x-layouts::auth.card>
+</x-layouts::auth.split>
