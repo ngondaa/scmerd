90adcf3e86fb2f61bca96ac8f5f3c2644c8d3fae
ngondaa <113019728+ngondaa@users.noreply.github.com>
2026-09-04 15:44:23 +0200

    Update auth.blade.php

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,4 +1,4 @@
-<x-layouts::auth.card :title="$title ?? null">
+<x-layouts::auth.card> :title="$title ?? null">
     <div class="min-h-screen bg-white px-4 py-10 text-black">
         <div class="mx-auto max-w-md pt-8 text-black">
             {{ $slot }}
